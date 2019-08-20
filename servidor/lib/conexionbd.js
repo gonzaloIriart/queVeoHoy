3b37fe28c8061ebf33fd2871943aa8ef95fdfb0e
GonzaloIriart <gonzaloiriart@gmail.com>
2019-08-19 22:18:28 -0300

    part1

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,11 +1,11 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 
-  port     : 
-  user     : 
-  password : 
-  database : 
+  host     : '127.0.0.1',
+  port     : '3306',
+  user     : 'root',
+  password : 'root',
+  database : 'peliculasdb'
 });
 
 module.exports = connection;
--- servidor/servidor.js
+++ servidor/servidor.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
+var controlador = require('../servidor/controladores/controlador');
 var app = express();
 
 app.use(cors());

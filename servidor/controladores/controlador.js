7c6e196a23e4db8590e1619d8278232d159bd867
GonzaloIriart <gonzaloiriart@gmail.com>
2019-08-20 23:58:24 -0300

    part2 done

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -1,8 +1,18 @@
 var connection = require('../lib/conexionbd');
 
 function peliculas(req, res){
-  
-    connection.query("SELECT * FROM pelicula", function (err, result, fields) {
+    let query= `SELECT * FROM pelicula `;
+    console.log(req.query.genero)
+    if(req.query.genero !== undefined){
+      query = query + `WHERE genero_id = ${req.query.genero} `; 
+    }if(req.query.titulo !== undefined){
+      query = query + `AND titulo LIKE "%${req.query.titulo}%" `;
+    }if(req.query.anio !== undefined){
+      query = query + `AND anio = ${req.query.anio} `;
+    }if(req.query.order !== undefined){
+      query = query +`ORDER BY ${req.query.order} `;
+    }
+    connection.query(query, function (err, result, fields) {
       if (err) throw err;
       var respuesta = {
         peliculas: result

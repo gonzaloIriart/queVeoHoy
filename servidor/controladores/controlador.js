34253a9a38988f0a473f0b0c6c04624cf12548b1
GonzaloIriart <gonzaloiriart@gmail.com>
2019-08-21 22:08:10 -0300

    fixed informacion de pelicula en controlador

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -2,34 +2,24 @@ var connection = require('../lib/conexionbd');
 
 function pelicula(req, res){
   var id = parseInt(req.params.id);
-  console.log(req.params.id);
-  let respuesta = new Object();
-  let query1 = `SELECT nombre from actor a
-   INNER JOIN actor_pelicula ap ON ap.actor_id = a.id
-   INNER JOIN pelicula p on p.id = ap.pelicula_id
-   WHERE p.id = ${id}`;
-   let query2 = `SELECT * from pelicula p
-   INNER JOIN genero g on p.genero_id = g.id
-   WHERE p.id = ${id}`
-  /*
-
-[{"id":401,"titulo":"Narc","genero_id":6,"anio":2002,"duracion":105,"director":"Joe Carnahan","fecha_lanzamiento":"2003-01-10T03:00:00.000Z","puntuacion":7,"poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYjVhMWI2ZjgtNzNiNy00Mjc2LTljZDQtNGYwMzYwMTlhZGFhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg","trama":"When the trail goes cold on a murder investigation of a policeman an undercover narcotics officer is lured back to the force to help solve the case.","actor_id":401,"pelicula_id":1,"nombre":"Dan Leis"},{"id":871,"titulo":"Narc","genero_id":6,"anio":2002,"duracion":105,"director":"Joe Carnahan","fecha_lanzamiento":"2003-01-10T03:00:00.000Z","puntuacion":7,"poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYjVhMWI2ZjgtNzNiNy00Mjc2LTljZDQtNGYwMzYwMTlhZGFhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg","trama":"When the trail goes cold on a murder investigation of a policeman an undercover narcotics officer is lured back to the force to help solve the case.","actor_id":871,"pelicula_id":1,"nombre":"Jason Patric"},{"id":1238,"titulo":"Narc","genero_id":6,"anio":2002,"duracion":105,"director":"Joe Carnahan","fecha_lanzamiento":"2003-01-10T03:00:00.000Z","puntuacion":7,"poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYjVhMWI2ZjgtNzNiNy00Mjc2LTljZDQtNGYwMzYwMTlhZGFhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg","trama":"When the trail goes cold on a murder investigation of a policeman an undercover narcotics officer is lured back to the force to help solve the case.","actor_id":1238,"pelicula_id":1,"nombre":"Lloyd Adams"},{"id":1397,"titulo":"Narc","genero_id":6,"anio":2002,"duracion":105,"director":"Joe Carnahan","fecha_lanzamiento":"2003-01-10T03:00:00.000Z","puntuacion":7,"poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYjVhMWI2ZjgtNzNiNy00Mjc2LTljZDQtNGYwMzYwMTlhZGFhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg","trama":"When the trail goes cold on a murder investigation of a policeman an undercover narcotics officer is lured back to the force to help solve the case.","actor_id":1397,"pelicula_id":1,"nombre":"Meagan Issa"}]
-  */
-  connection.query(query1, function (err, result, fields) {
+  let respuesta = {};
+  let query = `SELECT a.nombre as nombreActor,g.nombre,p.* from pelicula p
+  INNER JOIN actor_pelicula ap ON ap.pelicula_id = p.id
+  INNER JOIN genero g on g.id = p.genero_id
+  INNER JOIN actor a on a.id = ap.actor_id
+  WHERE p.id = ${id}`
+ 
+  connection.query(query, function (err, result, fields) {
     if (err) throw err;
-    //console.log(JSON.stringify(result));    
-    respuesta.actores = result;    
-  });
+    //console.log(JSON.stringify(result));  
+    respuesta.pelicula = result[0];
+    respuesta.actores = result.map(actor => actor.nombreActor);  
+    console.log(respuesta)    
+    res.send(JSON.stringify(respuesta));
+    });
+  }
 
-  connection.query(query2, function (err, result, fields) {
-    if (err) throw err;
-    //console.log(JSON.stringify(result));    
-    respuesta.pelicula = result;
-  });
-  console.log(respuesta)
-  res.send(JSON.stringify(respuesta));
-    
-}
+  
 
 function peliculas(req, res){
     let query= `SELECT * FROM pelicula `;
--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   port     : '3306',
   user     : 'root',
   password : 'root',
-  database : 'peliculasdb'
+  database : 'peliculasdb',
+  multipleStatements: true
 });
 module.exports = connection;
 

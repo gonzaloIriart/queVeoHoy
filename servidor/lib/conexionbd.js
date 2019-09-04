fe5398d1c15bb958ed2d2a4877bc920bf9017ad2
GonzaloIriart <gonzaloiriart@gmail.com>
2019-09-04 12:56:06 -0300

    database

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : '127.0.0.1',
+  host     : 'us-cdbr-iron-east-02.cleardb.net',
   port     : '3306',
-  user     : 'root',
-  password : 'root',
-  database : 'peliculasdb',
+  user     : 'b97a2c74b6f3b4',
+  password : '42e91a94',
+  database : 'heroku_366f6c23b35b624',
   multipleStatements: true
 });
 module.exports = connection;

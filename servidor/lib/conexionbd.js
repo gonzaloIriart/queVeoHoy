2a3504460410bff69fe97f45d8e04ace2a6b6a45
GonzaloIriart <gonzaloiriart@gmail.com>
2019-09-04 15:17:37 -0300

    a

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,12 +1,5 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-02.cleardb.net',
-  port     : '3306',
-  user     : 'b97a2c74b6f3b4',
-  password : '42e91a94',
-  database : 'heroku_366f6c23b35b624',
-  multipleStatements: true
-});
+var connection = mysql.createConnection('mysql://b97a2c74b6f3b4:42e91a94@us-cdbr-iron-east-02.cleardb.net/heroku_366f6c23b35b624?reconnect=true');
 module.exports = connection;
 

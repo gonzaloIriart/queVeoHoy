2c0882f1e02701d388e2086e90754fcbb46c5d58
GonzaloIriart <gonzaloiriart@gmail.com>
2019-09-09 00:23:49 -0300

    removed link

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection('mysql://b97a2c74b6f3b4:42e91a94@us-cdbr-iron-east-02.cleardb.net/heroku_366f6c23b35b624?reconnect=true');
+var connection = mysql.createConnection('');
 module.exports = connection;
 
